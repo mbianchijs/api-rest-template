b15b7c2062bb35cdcc8b0f4bf589dd5ba75a3d8d
Jose Martinez <mbianchijs@gmail.com>
2021-05-17 16:12:49 -0400

    Creacion metodos post, put, path, delete ctlr libros

--- controllers/libros.js
+++ controllers/libros.js
@@ -5,7 +5,7 @@ const librosGet = (req = request, res = response) => {
     const { method } = req;
     const { name, page = 1, limit = 10 } = req.query;
 
-    res.json({
+    res.status(200).json({
         msg: `Método usado: ${ method }`,
         nombre: name,
         pagina: page,
@@ -19,14 +19,73 @@ const libroGet = (req = request, res = response) => {
     const { method } = req;
     const idLibro = req.params.id;
 
-    res.json({
+    res.status(200).json({
         msg: `Método usado: ${ method }`,
         idLibro: `El libro a consultar tiene el ID ${ idLibro }`
     })
 
 }
 
+const librosPost = (req = request, res = response) => {
+
+    const { method } = req;
+    const { nombre, autor, publicacion } = req.body;
+
+    res.status(200).json({
+        msg: `Método usado: ${ method }`,
+        nombre,
+        autor,
+        publicacion
+    })
+
+}
+
+const librosPut = (req = request, res = response) => {
+
+    const { method } = req;
+    const { id, state } = req.query;
+
+    res.status(200).json({
+        msg: `Método usado: ${ method }`,
+        idModificado: id,
+        nuevoEstado: state
+    })
+
+}
+
+const librosPatch = (req = request, res = response) => {
+
+    const { method } = req;
+    const idLibro = req.params.id;
+    const { nombre, autor, publicacion } = req.body;
+
+    res.status(200).json({
+        msg: `Método usado: ${ method }`,
+        idModificado: idLibro,
+        nuevoNombre: nombre,
+        nuevoAutor: autor,
+        nuevaPublicacion: publicacion
+    })
+
+}
+
+const librosDelete = (req = request, res = response) => {
+
+    const { method } = req;
+    const idLibro = req.params.id;
+
+    res.status(200).json({
+        msg: `Método usado: ${ method }`,
+        idEliminado: idLibro,
+    })
+
+}
+
 module.exports = {
     librosGet,
-    libroGet
+    libroGet,
+    librosPost,
+    librosPut,
+    librosPatch,
+    librosDelete
 }
--- routes/libros.js
+++ routes/libros.js
@@ -3,5 +3,9 @@ const ctlLibros = require('../controllers/libros');
 
 router.get('/', ctlLibros.librosGet);
 router.get('/:id', ctlLibros.libroGet);
+router.post('/', ctlLibros.librosPost);
+router.put('/', ctlLibros.librosPut);
+router.patch('/:id', ctlLibros.librosPatch);
+router.delete('/:id', ctlLibros.librosDelete);
 
 module.exports = router;
